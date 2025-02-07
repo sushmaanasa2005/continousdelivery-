669f616b51dce9caddb49f2beb74bfa481f9e32a
unknown <2200030311@kluniversity.in>
2025-02-07 21:48:44 +0530

    first commit

---
+++ add.py
@@ -0,0 +1,16 @@
+#This program adds two numbers
+
+num1=1.5
+num2=6.3
+
+#add two numbers
+sum=num1+num2
+#display the sum
+print('The Sum of{0} and {1} is {2}'.format(num1,num2,sum)
+git status
+on branch main
+your branchis up-to-date with 'origin/main'
+git add.
+untracked files:
+ (use "git add <file>.." to include in what will be committed)
+add.py
